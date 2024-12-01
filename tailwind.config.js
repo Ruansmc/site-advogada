823108cb1a69fa912f97c9642bb939ea2e553f5d
Ruan Carvalho <86540031+Ruansmc@users.noreply.github.com>
2024-12-01 11:48:06 -0300

    Adiciona .gitignore para ignorar node_modules e remove node_modules do controle de versão

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Caso o seu HTML esteja no diretório raiz
+    "./src/**/*.{html,js}", // Inclua todos os arquivos HTML e JS dentro da pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
